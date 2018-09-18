fd8639cf88180add0a1edd397b4fa10dc56df48b
Riad Shash <riadshash@yahoo.com>
2018-09-17 18:49:52 -0600

    Worked on main.cpp

--- RadixSort/main.cpp
+++ RadixSort/main.cpp
@@ -1,1 +1,41 @@
+/*
+Riad Shash (Ray)
+Selection Sort C++ implementation
+*/
+
+#include <iostream>
+
+void swap(int &num1, int &num2);
+
+void printArray(int array[], const int &arraySize);
+
+int main(void){
+
+    int ary1[10] = {9,4,2,5,7,8,1,0,6,3};
+    std::cout<<"Selection Sort \n";
+    std::cout<<"Array: { 9,4,2,5,7,8,1,0,6,3 } \n";
+    
+    std::cout<<"After sorting (using Selection Sort): ";
+    printArray(ary1,10);
+    std::cin.ignore();
+
+    return 0;
+}
+
+//Swaps two given array elements
+void swap(int &num1, int &num2){
+    double temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+void printArray(int array[], const int &arraySize){
+    std::cout<<"{ ";
+    for(int i = 0; i < arraySize; i++){
+        std::cout<<array[i] <<" ";
+    }
+    std::cout<<"}";
+}
+
+
 
