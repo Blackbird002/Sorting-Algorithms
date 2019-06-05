b2527ca67f00a1501af4b1b3ca740b7b60ce59a3
Riad Shash <riadshash@yahoo.com>
2019-06-05 15:45:55 -0600

    template implementation of Insertion Sort & starts at front of array

--- InsertionSort/main.cpp
+++ InsertionSort/main.cpp
@@ -4,55 +4,55 @@ Insertion Sort C++ implementation
 */
 
 #include <iostream>
+#include <cassert>
 
-void swap(int &num1, int &num2);
-void insertionSort(int array[], const int &arraySize);
-void printArray(int array[], const int &arraySize);
-
-int main(void){
-
-    //Needs an array +1 bigger than the size
-    int ary1[10+1] = {9,4,2,5,7,8,1,0,6,3};
-    std::cout<<"Insertion Sort \n";
-    std::cout<<"Array: { 9,4,2,5,7,8,1,0,6,3 } \n";
-    insertionSort(ary1,10);
-    std::cout<<"After sorting (using Insertion Sort): ";
-    printArray(ary1,10);
-    std::cin.ignore();
-
-    return 0;
+template <class T>
+void insertionSort(T array[], const int& arraySize) {
+	T key;
+	int j, i;
+	for (j = 1; j < arraySize; j++) {
+		key = array[j];
+		i = j - 1;
+		while (i >= 0 && array[i] > key) {
+			array[i + 1] = array[i];
+			i--;
+		}
+		array[i + 1] = key;
+	}
 }
 
-//Swaps two given array elements
-void swap(int &num1, int &num2){
-    double temp = num1;
-    num1 = num2;
-    num2 = temp;
+template <class T>
+void test(int actual[], int expected[], const int& n)
+{
+	for (int i = 0; i < n; i++)
+		assert(actual[i] == expected[i]);
+	std::cout << "Test passed! \n";
 }
 
-void printArray(int array[], const int &arraySize){
-    std::cout<<"{ ";
-    for(int i = 0; i < arraySize; i++){
-        std::cout<<array[i] <<" ";
-    }
-    std::cout<<"}";
+int main(void) {
+	int actual[10] = { 9,4,2,5,7,8,1,0,6,3 };
+	int expected[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	std::cout << "Insertion Sort \n";
+	insertionSort<int>(actual, 10);
+	test<int>(actual, expected, 10);
+	return 0;
 }
 
 /*
 Insertion Sort algorithm
 Needs an array +1 bigger than the size
-*/
-void insertionSort(int array[], const int &arraySize)
-{
-    int j,save;
-    for(int k = arraySize - 1; k >= 0; k--){
-        j = k + 1;
-        save = array[k];
-        array[arraySize] = save;
-        while (save > array[j]){
-            array[j-1] = array[j];
-            j = j + 1;
-        }
-        array[j-1] = save;
-    }
+void insertionSort(int array[], const int& arraySize){
+	int j, save;
+	for (int k = arraySize - 1; k >= 0; k--) {
+		j = k + 1;
+		save = array[k];
+		array[arraySize] = save;
+		while (save > array[j]) {
+			array[j - 1] = array[j];
+			j = j + 1;
+		}
+		array[j - 1] = save;
+	}
 }
+*/
+
