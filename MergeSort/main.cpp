d0021d6c6dd397e94ca6c2a76d070191fe07a229
Riad Shash <riadshash@yahoo.com>
2019-06-14 00:03:14 -0600

    Added another Merge Sort test case (10,000 random #s)

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -6,6 +6,12 @@ Merge Sort C++
 #include <iostream>
 #include <cassert>
 #include <cmath>
+#include <random>
+#include <chrono>
+#include <algorithm>
+
+//The number of random numbers to generate...
+const int MaxNum = 10000;
 
 template <class T>
 void printArray(T array[], const int& arraySize) {
@@ -18,7 +24,7 @@ void printArray(T array[], const int& arraySize) {
 
 
 template <class T>
-void compareArray(int actual[], int expected[], const int& n) {
+void compareArray(T actual[], T expected[], const int& n) {
 	for (int i = 0; i < n; i++)
 		assert(actual[i] == expected[i]);
 }
@@ -33,12 +39,12 @@ void merge(T array[], int p, int q, int r, T sentinelVal) {
 	int n1 = q - p + 1;
 	int n2 = r - q;
 	//std::cout << "n1 = " << n1 << " n2 = " << n2 << std::endl;
-	T *L = new T[n1 + 1];
-	T *R = new T[n2 + 1];
+	T* L = new T[n1 + 1];
+	T* R = new T[n2 + 1];
 	for (int i = 0; i < n1; i++)
 		L[i] = array[p + i];
 	for (int j = 1; j <= n2; j++)
-		R[j-1] = array[q + j];
+		R[j - 1] = array[q + j];
 	L[n1] = sentinelVal;
 	R[n2] = sentinelVal;
 	//printArray<T>(L, n1 + 1);
@@ -64,7 +70,7 @@ void mergeSort(T array[], int p, int r, T sentinelVal) {
 	if (p < r) {
 		int q = floor((p + r) / 2);
 		mergeSort<T>(array, p, q, sentinelVal);
-		mergeSort<T>(array, q+ 1, r, sentinelVal);
+		mergeSort<T>(array, q + 1, r, sentinelVal);
 		merge<T>(array, p, q, r, sentinelVal);
 	}
 }
@@ -85,9 +91,39 @@ void testMergeSort() {
 	std::cout << "Merge Sort Test passed!\n";
 }
 
+void testMergeSortCase2() {
+	double randomNum = 0;
+	double actual[MaxNum];
+	double expected[MaxNum];
+
+	//Generates a seed based on elapsed time since Coordinated Universal Time (UTC) 
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	//Uses the 32 - bit Mersenne Twister Random Number Engine
+	std::mt19937 randomEngine(seed);
+	std::uniform_real_distribution<double> myDistribution(-100000, 100000);
+
+	for (int i = 0; i < MaxNum; i++)
+	{
+		randomNum = myDistribution(randomEngine);
+		actual[i] = randomNum;
+		expected[i] = randomNum;
+	}
+	//Actual array is sorted here
+	mergeSort<double>(actual, 0, MaxNum - 1, 200000);
+
+	//Expected Array is sorted here
+	int n = sizeof(expected) / sizeof(expected[0]);
+	std::sort(expected, expected + n);
+
+	//Checks if array contents are exactly in the same order
+	compareArray<double>(actual, expected, MaxNum);
+	std::cout << "Merge Sort Case 2 passed!\n";
+}
+
 int main(void) {
 	std::cout << "Merge Sort \n";
 	testMerge();
 	testMergeSort();
+	testMergeSortCase2();
 	return 0;
 }
